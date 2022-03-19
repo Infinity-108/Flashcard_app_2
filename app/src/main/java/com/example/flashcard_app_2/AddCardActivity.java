c307020ea467c1dd735f33874f51d69e17a45016
Infinity-108 <ugonna.akali@gmail.com>
2022-03-19 00:35:12 -0500

    Initial Commit

--- app/src/main/java/com/example/flashcard_app_2/AddCardActivity.java
+++ app/src/main/java/com/example/flashcard_app_2/AddCardActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddCardActivity extends AppCompatActivity {
 
@@ -14,6 +16,27 @@ public class AddCardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_card);
 
+
+        EditText question_edit = findViewById(R.id.question_edittext);
+        EditText answer1_edit = findViewById(R.id.answer1_edittext);
+        EditText answer2_edit = findViewById(R.id.answer2_edittext);
+        EditText answer3_edit = findViewById(R.id.answer3_edittext);
+
+
+        //this accounts for receiving data from the Main Activity
+        String Original_question = getIntent().getStringExtra("Original_question");
+        String Original_answer1 = getIntent().getStringExtra("Original_answer1");
+        String Original_answer2 = getIntent().getStringExtra("Original_answer2");
+        String Original_answer3 = getIntent().getStringExtra("Original_answer3");
+
+
+        //answer 1 is the correct answer
+        question_edit.setText(Original_question);
+        answer1_edit.setText(Original_answer1);
+        answer2_edit.setText(Original_answer2);
+        answer3_edit.setText(Original_answer3);
+
+
         findViewById(R.id.cancel_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -21,18 +44,36 @@ public class AddCardActivity extends AppCompatActivity {
             }
         });
 
-        EditText question_edit = findViewById(R.id.question_edittext);
-        EditText answer_edit = findViewById(R.id.answer_edittext);
+
 
 
         findViewById(R.id.save_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //for an empty question and answer box
+                Toast toast = Toast.makeText(getApplicationContext(), "Must Enter Both Question and three Answer!",
+                        Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 500);
+
+
                 Intent data  = new Intent();
                 data.putExtra("the_question",question_edit.getText().toString());
-                data.putExtra("the_answer",answer_edit.getText().toString());
+                data.putExtra("the_answer1",answer1_edit.getText().toString());
+                data.putExtra("the_answer2",answer2_edit.getText().toString());
+                data.putExtra("the_answer3",answer3_edit.getText().toString());
+
                 setResult(RESULT_OK, data);
-                finish();
+
+                if(!question_edit.getText().toString().isEmpty() &&
+                        !answer1_edit.getText().toString().isEmpty() &&
+                        !answer2_edit.getText().toString().isEmpty() &&
+                        !answer3_edit.getText().toString().isEmpty()) {
+                    finish();
+                }
+
+                else{
+                    toast.show();
+                }
             }
         });
 
--- app/src/main/java/com/example/flashcard_app_2/MainActivity.java
+++ app/src/main/java/com/example/flashcard_app_2/MainActivity.java
@@ -14,26 +14,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView Question = findViewById(R.id.question_textview);
+        TextView Answer = findViewById(R.id.answer_textview); //comment out later
+        TextView answer1 = findViewById(R.id.answer1_textview);
+        TextView answer2 = findViewById(R.id.answer2_textview);
+        TextView answer3 = findViewById(R.id.answer3_textview);
+
         findViewById(R.id.add_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+
                 MainActivity.this.startActivityForResult(intent,100);
             }
         });
 
-        TextView Question = findViewById(R.id.question_textview);
-        TextView Answer = findViewById(R.id.answer_textview);
-
-
-
-        Question.setOnClickListener(new View.OnClickListener(){
+        //When this button is clicked, the user will be able to edit previous input.
+        findViewById(R.id.edit_button).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Question.setVisibility(View.INVISIBLE);
-                Answer.setVisibility(View.VISIBLE);
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                intent.putExtra("Original_question",Question.getText().toString());
+                //intent.putExtra("Original_answer",Answer.getText().toString()); //delete after!!
+                intent.putExtra("Original_answer1",answer1.getText().toString());
+                intent.putExtra("Original_answer2",answer2.getText().toString());
+                intent.putExtra("Original_answer3",answer3.getText().toString());
+                MainActivity.this.startActivityForResult(intent,100);
             }
         });
+
+
+
+
+        //DELETE LATER or comment out
+//        Question.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v) {
+//                Question.setVisibility(View.INVISIBLE);
+//                Answer.setVisibility(View.VISIBLE);
+//            }
+//        });
 
     }
 
@@ -43,14 +63,24 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         TextView Question = findViewById(R.id.question_textview);
-        TextView Answer = findViewById(R.id.answer_textview);
+        //TextView Answer = findViewById(R.id.answer_textview);
+        TextView answer1 = findViewById(R.id.answer1_textview);
+        TextView answer2 = findViewById(R.id.answer2_textview);
+        TextView answer3 = findViewById(R.id.answer3_textview);
+
 
         if (requestCode == 100 && data != null) { //needs to match so they know which activity to call?
             String the_question = data.getExtras().getString("the_question"); // 'string1' needs to match key used in Intent
-            String the_answer = data.getExtras().getString("the_answer");
+            //String the_answer = data.getExtras().getString("the_answer");
+            String the_answer1 = data.getExtras().getString("the_answer1");
+            String the_answer2 = data.getExtras().getString("the_answer2");
+            String the_answer3 = data.getExtras().getString("the_answer3");
 
             Question.setText(the_question);
-            Answer.setText(the_answer);
+            //Answer.setText(the_answer);
+            answer1.setText(the_answer1);
+            answer2.setText(the_answer2);
+            answer3.setText(the_answer3);
         }
 
 
